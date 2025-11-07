6aeb71e568c0d7b0794ea9c6994749b04d3a4849
hoangtm <hoangtranminh@tech.admicro.vn>
2025-11-07 13:48:04 +0700

    fix: fix empty response error

--- src/qa_sales/modules/evaluators.py
+++ src/qa_sales/modules/evaluators.py
@@ -143,11 +143,11 @@ class ScriptEvaluator:
         )
 
         # Invoke LLM
-        prompt_text = prompt_text.invoke(
+        prompt_text = await prompt_text.ainvoke(
             {"sale_texts": utterances, "step_detail": self.step_detail}
         )
-        response = self.llm.invoke(prompt_text)
-        parsed_response = self.classify_output_parser.invoke(response)
+        response = await self.llm.ainvoke(prompt_text)
+        parsed_response = await self.classify_output_parser.ainvoke(response)
         parsed_response = [
             item.model_dump() for item in parsed_response.utterance_holders
         ]
@@ -341,11 +341,11 @@ class ScriptEvaluator:
                     "format_instructions": self.eval_output_parser.get_format_instructions()
                 },
             )
-            eval_prompt = eval_prompt.invoke(
+            eval_prompt = await eval_prompt.ainvoke(
                 {"sale_texts": sale_texts, "step_detail": self.step_detail}
             )
-            response = self.llm.invoke(eval_prompt)
-            parsed_response = self.eval_output_parser.invoke(response)
+            response = await self.llm.ainvoke(eval_prompt)
+            parsed_response = await self.eval_output_parser.ainvoke(response)
             eval_results = [item.model_dump() for item in parsed_response.results]
 
             response_content = response.content
